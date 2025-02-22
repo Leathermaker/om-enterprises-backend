2fa718d7baeecf9d8642e6022e77337b57578cc1
deepakcodez <deepak2603dk@gmail.com>
2025-02-22 12:01:07 +0530

    update delete api

--- Controller/plan.controller.js
+++ Controller/plan.controller.js
@@ -1,7 +1,7 @@
 import { Plan } from "../models/plan.model.js";
 
 const addPlan = async (req, res) => {
-    const { title, price, descriptions,category } = req.body;
+    const { title, price, descriptions, category } = req.body;
     console.log("data", req.body);
 
     try {
@@ -9,7 +9,7 @@ const addPlan = async (req, res) => {
             return res.status(400).json({ message: "Please fill all the fields" })
         }
         const newPlan = await Plan.create({ title, category, price, descriptions });
-        return res.status(200).json({message: "new plan created successfully", plan: newPlan})
+        return res.status(200).json({ message: "new plan created successfully", plan: newPlan })
     } catch (error) {
         console.log(error);
         return res.status(500).json({ message: "Internal server error" })
@@ -17,100 +17,99 @@ const addPlan = async (req, res) => {
 };
 
 
-const getPlan = async(req,res)=>{
+const getPlan = async (req, res) => {
     try {
         const { category } = req.params
         console.log(category)
-         const allPlans = await Plan.find({ category })
-         if(allPlans){
-           return res.status(200).json({
-                msg:"Successfully done",
-                plans:allPlans
-            })
-         }
-         return res.status(402).json({
-            msg:"No such category present"
-         })
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({
-            msg:"Unexpected Error",           
-        })
-    }
-}
-
-
-const deletePlan = async(req,res)=>{
-    try {
-        const { id } = req.body
-        
-        const response = await Plan.findOne({_id  : id})
-        console.log(response)
-
-        if(!response){
-            return res.status(402).json({
-                msg:"Plan is not available"
-            })
-        }
-        const confirm = await Plan.deleteOne({ _id : id })
-
-        console.log(confirm)
-        if(confirm.deleteCount > 0){
+        const allPlans = await Plan.find({ category })
+        if (allPlans) {
             return res.status(200).json({
-                msg:"Successfully deleted"
+                msg: "Successfully done",
+                plans: allPlans
             })
         }
         return res.status(402).json({
-            msg:"Not deleted"
+            msg: "No such category present"
         })
     } catch (error) {
         console.log(error)
         return res.status(500).json({
-            msg:"Unexpected Error"
+            msg: "Unexpected Error",
         })
     }
 }
 
 
-const updatePlan = async(req,res)=>{
+const deletePlan = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const response = await Plan.findOne({ _id: id })
+        console.log(response)
+
+        if (!response) {
+            return res.status(400).json({   
+                msg: "Plan is not available"
+            })
+        }
+        const planDeleted = await Plan.deleteOne({ _id: id })
+        if(!planDeleted)    {
+            return res.status(400).json({
+                msg: "Plan is not deleted"
+            })
+        }
+
+        return res.status(200).json({
+            msg: "plan deleted successfully",
+            deletedPlan : planDeleted
+        })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            msg: "Unexpected Error"
+        })
+    }
+}
+
+
+const updatePlan = async (req, res) => {
     try {
         const { id } = req.params
-        const { title,category,price,descriptions } = req.body
+        const { title, category, price, descriptions } = req.body
 
-        const response = await Plan.findOne({ _id : id })
+        const response = await Plan.findOne({ _id: id })
 
-        if(!response){
+        if (!response) {
             return res.status(402).json({
-                msg:"Plan is not present"
+                msg: "Plan is not present"
             })
         }
 
         const updateObj = {
             title: title || response.title,
             category: category || response.category,
-            price : price || response.price,
-            descriptions : descriptions || response.descriptions
+            price: price || response.price,
+            descriptions: descriptions || response.descriptions
         }
 
         const result = await Plan.updateOne(
-            {_id : id},
-            {$set : updateObj}
+            { _id: id },
+            { $set: updateObj }
         )
 
-        if(result.modifiedCount > 0){
+        if (result.modifiedCount > 0) {
             return res.status(200).json({
-                msg:"Successfully Updated"
+                msg: "Successfully Updated"
             })
         }
 
         return res.status(402).json({
-            msg:"Provided data is already exist"
+            msg: "Provided data is already exist"
         })
 
     } catch (error) {
         console.log(error)
         return res.status(402).json({
-            msg:"Unexpected Error"
+            msg: "Unexpected Error"
         })
     }
 }
